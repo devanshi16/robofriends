880d699417910268829ef936a116ccd745dfc257
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-07-11 00:04:47 -0400

    initial commit

---
+++ build/precache-manifest.f32d59ba16f0e77eaf1d501384ada823.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e84ec489f111c08f7350e775dfc6f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3342b7cf82decde449ff",
+    "url": "/static/css/main.0a559fd1.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3342b7cf82decde449ff",
+    "url": "/static/js/main.664915ed.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
